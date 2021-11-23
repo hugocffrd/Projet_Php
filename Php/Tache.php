538febada4a007adcc13e68e7d6f561e0a58d184
Hugo Coiffard <76486066+hugodu63@users.noreply.github.com>
2021-11-23 17:47:51 +0100

    Update Tache et test

--- Php/Tache.php
+++ Php/Tache.php
@@ -7,9 +7,16 @@ class Tache
     public $Texte;
     public $DateFin;
 
-    public function __construct()
+    public function __construct($IdT,$Nom,$Texte,$DateFin)
     {
+        $this->IdT=$IdT;
+        $this->Nom=$Nom;
+        $this->Texte=$Texte;
+        $this->DateFin=$DateFin;
+    }
 
+    public function show(){
+        print $this->IdT." ".$this->Nom.",".$this->Texte.",".$this->DateFin."<br>";
     }
 
 }
