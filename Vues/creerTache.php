7ca737b79f1163ea946e107de34246c320cd78e4
AkyroSensei <mathyslioson@gmail.com>
2021-11-26 20:41:57 +0100

    Changement du style de la page

--- Vues/creerTache.php
+++ Vues/creerTache.php
@@ -1,19 +1,41 @@
 <!DOCTYPE html>
-<html lang="FR">
+<html lang="FR" >
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Creation Tache</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
+<div class="container">
+    <form action="creerTache.php" method="post">
+        <div class="row">
+            <h4>Tâche</h4>
+            <div class="input-group input-group-icon" >
+                <input type="number" placeholder="id" name="id"/>
+                <div class="input-icon"><i class="fa fa-user"></i></div>
+            </div>
+            <div class="input-group input-group-icon">
+                <input type="text" placeholder="Nom de votre tâche" name="nom"/>
+                <div class="input-icon"><i class="fa fa-envelope"></i></div>
+            </div>
+            <div class="input-group input-group-icon">
+                <input type="text" placeholder="Description" name="desc"/>
+                <div class="input-icon"><i class="fa fa-key"></i></div>
+            </div>
+            <div class="input-group input-group-icon">
+                <input type="date" placeholder="date de fin" name="date"/>
+                <div class="input-icon"><i class="fa fa-key"></i></div>
+            </div>
+            <div class="input-group input-group-icon">
+                <input type="submit" value="Valider"/>
+                <div class="input-icon"><i class="fa fa-key"></i></div>
+            </div>
 
-<form action="creerTache.php" method="post">
-    <p>Id de la tache : <input type="text" name="id" /></p>
-    <p>Nom de la tache : <input type="text" name="nom" /></p>
-    <p>Description de la tache : <input type="text" name="desc" /></p>
-    <p>date de création de la tache : <input type="date" name="date" /></p>
-    <p><input type="submit" value="Valider"></p>
-</form>
 
+        </div>
+    </form>
+</div>
 <?php
 require_once "../Php/Tache.php";
 require_once '../Php/TacheGateway.php';
@@ -25,10 +47,12 @@ $date = isset($_POST['date']) ? $_POST['date'] : "";
 $tacheCreer=new Tache($id,$nom,$desc,$date);
 $Tgateway=new TacheGateway("mysql:host=localhost;dbname=dbroot","root","");
 $Tgateway->insertTache($tacheCreer);
+
+//$Tgateway=new TacheGateway("mysql:host=localhost;dbname=dbroot","root","");
+//$Tgateway->insertTache($tacheCreer);
 print("ID : $id <br>Nom : $nom <br>Description : $desc<br>Date : $date");
 
 
 ?>
-
 </body>
 </html>
