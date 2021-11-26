f8d4413acc52ec77b7491d537aaa8ca2045c445f
Hugo Coiffard <76486066+hugodu63@users.noreply.github.com>
2021-11-26 16:39:35 +0100

    Gateway et test

--- Php/TacheGateway.php
+++ Php/TacheGateway.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'Connection.php';
-require_once  'Tache.php';
+require_once 'Tache.php';
 
 class TacheGateway
 {
@@ -12,17 +12,17 @@ class TacheGateway
         $this->connect=new Connection($dsn,$user,$pwd);
         }
 
-    public	function CreateTache($id,$nom,$texte,$dateFin){
-            $query="INSERT INTO TACHE (Nom,Texte,dateFin)VALUES($id,$nom,$texte,$dateFin)";
-            $this->connect->executeQuery($query,array(
-                ":IdT"=>array($id,PDO::PARAM_STR),
-                ":Nom"=>array($nom,PDO::PARAM_STR),
-                ":Texte"=>array($texte,PDO::PARAM_STR),
-                ":DateFin"=>array($dateFin,PDO::PARAM_STR)));
+    public	function insertTache(Tache $tache){
+        $query="INSERT INTO tache VALUES(:IdT,:Nom,:Texte,:DateFin)";
+        $this->connect->executeQuery($query,array(
+            ":IdT"=>array($tache->IdT,PDO::PARAM_STR),
+            ":Nom"=>array($tache->Nom,PDO::PARAM_STR),
+            ":Texte"=>array($tache->Texte,PDO::PARAM_STR),
+            ":DateFin"=>array($tache->DateFin,PDO::PARAM_STR)));
     }
 
-    public function findById ($id):array{
-        $query="SELECT * FROM tache WHERE IdT=:id";
+    public function findById ($id):Tache{
+        $query="SELECT * FROM Tache WHERE IdT=:id";
         $this->connect->executeQuery($query,array(
             ":IdT"=>array($id,PDO::PARAM_STR),
             ":Nom"=>array($id,PDO::PARAM_STR),
@@ -30,8 +30,8 @@ class TacheGateway
             ":DateFin"=>array($id,PDO::PARAM_STR)));
         $results=$this->connect->getResults();
         foreach ($results as $row){
-            $tabTache[]=new Tache($row ["IdT"]);}
-        return $tabTache;
+            $newTache=new Tache($row ["IdT"],$row ["Nom"],$row ["Texte"],$row ["DateFin"]);}
+        return $newTache;
     }
 
 }
--- Tests/TacheTest.php
+++ Tests/TacheTest.php
@@ -1,24 +1,22 @@
 <html>
     <body>
-        <h1>Test création tâche</h1>
-        <?php
+    <h1>Test création tâche</h1>
+    <?php
 
-            class TacheTest
-            {
+        require_once "../Php/Tache.php";
+        require_once "../Php/TacheGateway.php";
 
-
-                public function TCreate ()
-                {
-                    $tacheTest = new Tache(003, "Nourrir les hugo", "Il faut les nourrir pour les manger après", 10 / 12 / 2021);
-                    $tacheTest->show();
-                }
+        $pdoExcp=new Exception();
+        $tacheTest = new Tache(004, 'Nourrir les hugo', 'Il faut les nourrir pour les manger après', '10 / 12 / 2021');
+        $tacheTest->show();
 
 
-            }
+        /*$Tgateway=new TacheGateway("mysql:host=localhost;dbname=dbroot","root","");
+        $Tgateway->insertTache($tacheTest);*/
 
-
-
-        ?>
+        $findTache=$Tgateway->findById(004);
+        $findTache->show();
+    ?>
     </body>
 </html>
 
