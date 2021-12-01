e6b09d846c9dc5311076d90491384d609107913e
Mathys <mathyslioson@gmail.com>
2021-12-01 10:14:02 +0100

    Ajout de test conexion

--- Tests/TacheTest.php
+++ Tests/TacheTest.php
@@ -11,16 +11,18 @@
         $tacheTest = new Tache(004, 'Nourrir les hugo', 'Il faut les nourrir pour les manger après', '10 / 12 / 2021');
         $tacheTest->show();
 
+        
+    
+
         //Test création tache dans base de données
         $Tgateway=new TacheGateway("mysql:host=localhost;dbname=dbroot","root","");
         //$Tgateway->insertTache($tacheTest);
-
         //Test recherche d'une tache avec l'identifiant
         $tabFindTache[]=$Tgateway->findById(005);
         foreach ($tabFindTache as $tab){
             foreach ($tab as $tache){
                 $tache->show();}
-        }
+        } 
     ?>
     </body>
 </html>
---
+++ Tests/Testconnection.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+    <?php
+    require_once '../Php/Connection.php';
+    $con = new Connection("mysql:host=localhost;dbname=dbroot", "root", "");
+    $query = "SELECT COUNT(*) FROM tache WHERE IdT=:IdT";
+    $con->executeQuery($query, array(":IdT" => array($IdT, PDO::PARAM_STR)));
+    $results = $con->getResults();
+    foreach ($results as $row){
+        print("\t" . $row)
+    }
+    ?>
+
+
+</body>
+
+</html>
