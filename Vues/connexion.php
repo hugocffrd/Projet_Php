49b64e2a2edf041c227a4690dce6165dfbe0c5b5
Mathys <mathyslioson@gmail.com>
2021-12-01 10:22:14 +0100

    changelent conection

--- Vues/connexion.php
+++ Vues/connexion.php
@@ -1,22 +1,26 @@
-<html>
-    <head>
-       <meta charset="utf-8">
-        <link rel="stylesheet" href="styleCo.css" media="screen" type="text/css" />
-    </head>
-    <body>
-        <div id="container">
-            
-            <form action="verification.php" method="POST">
-                <h1>Connexion</h1>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+<!DOCTYPE html>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="styleCo.css" media="screen" type="text/css" />
+</head>
 
-                <input type="submit" id='submit' value='LOGIN' >
-            </form>
-        </div>
-    </body>
+<body>
+    <div id="container">
+
+        <form action="verification.php" method="POST">
+            <h1>Connexion</h1>
+
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+            <input type="submit" id='submit' value='LOGIN'>
+        </form>
+    </div>
+</body>
+
 </html>
