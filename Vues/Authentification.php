9edb070452eec481b27bf198bef419725edbc81b
Mathys <mathyslioson@gmail.com>
2021-12-04 17:13:16 +0100

    connection marche enfin

--- Vues/Authentification.php
+++ Vues/Authentification.php
@@ -6,18 +6,18 @@ if (isset($_POST['mail']) && isset($_POST['password'])) {
 
     $mail = htmlspecialchars($_POST['mail']);
     $password = htmlspecialchars($_POST['password']);
-    $connect = new Connection("mysql:host=localhost;dbname=dbroot", "root", "");
+    $connect = new PDO("mysql:host=localhost;dbname=dbroot", "root", "");
 
-    $check = $connect->prepare("SELECT Nom,Mail,Pwd from utilisateur where Mail = ?");
+    $check = $connect->prepare('SELECT Nom , Mail, Pwd from utilisateur where Mail = ?');
     $check->execute(array($mail));
     $data = $check->fetch();
     $row = $check->rowCount();
 
     if ($row == 1) {
         if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-            $password = hash('sha256', $password);
+            // $password = hash('sha256', $password);
 
-            if ($data['Pwd'] == $password) {
+            if ($password  == $data['Pwd']) {
                 $_SESSION['user'] = $data['Nom'];
                 header('Location:accueil.php');
             } else header('Location:connexion.php?login_err=password');
