6b790750321a9664b260216ae5b15bf4ebfe1b00
Mathys <mathyslioson@gmail.com>
2021-12-05 17:59:17 +0100

    changement du style de la vue conexion

--- Vues/connexion.php
+++ Vues/connexion.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="styleCo.css" media="screen" type="text/css" />
 </head>
 
@@ -54,5 +55,78 @@
         </form>
     </div>
 </body>
+
+<style>
+    body {
+        background: #90cbff;
+    }
+
+    #container {
+        width: 400px;
+        margin: 0 auto;
+        margin-top: 10%;
+    }
+
+    form {
+        width: 100%;
+        padding: 30px;
+        border: 1px solid #f1f1f1;
+        background: #fff;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+
+    #container h1 {
+        align-self: center;
+        width: fit-content;
+        margin: 0 auto;
+        padding-bottom: 10px;
+    }
+
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=mail],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=number],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=submit] {
+        background-color: #53af57;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+    }
+
+    input[type=submit]:hover {
+        background-color: white;
+        color: #53af57;
+        border: 1px solid #53af57;
+    }
+</style>
 
 </html>
