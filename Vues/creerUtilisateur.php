e0afcb14ff5cb6c0e24690ae7ea0b1b17828d56a
Mathys <mathyslioson@gmail.com>
2021-12-05 17:43:11 +0100

    changement du style de la page elle est plus styler maintenant

--- Vues/creerUtilisateur.php
+++ Vues/creerUtilisateur.php
@@ -2,13 +2,11 @@
 
 <head>
     <meta charset="utf-8">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styleCo.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div id="container">
+    <div class="container-sm">
         <?php
         if (isset($_GET['reg_err'])) {
             $err = htmlspecialchars($_GET['reg_err']);
@@ -83,9 +81,82 @@
             <input type="password" placeholder="Entrer le mot de passe" name="password" required>
             <label><b>Confirmer le mot de passe</b></label>
             <input type="password" placeholder="Retapper le mot de passe" name="password_retype" required>
-            <input type="submit" id='submit' value='Register'>
+            <input type="submit" id='submit' value="S'inscrire">
         </form>
     </div>
 </body>
+
+<style>
+    body {
+        background: #90cbff;
+    }
+
+    #container {
+        width: 400px;
+        margin: 0 auto;
+        margin-top: 10%;
+    }
+
+    form {
+        width: 100%;
+        padding: 30px;
+        border: 1px solid #f1f1f1;
+        background: #fff;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+
+    #container h1 {
+        align-self: center;
+        width: fit-content;
+        margin: 0 auto;
+        padding-bottom: 10px;
+    }
+
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=mail],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=number],
+    input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+    input[type=submit] {
+        background-color: #53af57;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+    }
+
+    input[type=submit]:hover {
+        background-color: white;
+        color: #53af57;
+        border: 1px solid #53af57;
+    }
+</style>
 
 </html>
